74c0cc313befa57083093616bba45e6d174992b6
Admin <gc974517@ohio.edu>
2019-10-19 18:32:15 -0400

    Small UI updates: Added search bar (non-functional) and button.

--- catmap/app/src/main/java/com/example/catmap/MainActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MainActivity.java
@@ -2,31 +2,17 @@ package com.example.catmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ListView search_results;
-    ArrayAdapter<String> adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        search_results = (ListView) findViewById(R.id.search_results);
-
-        ArrayList<String> arrayResults = new ArrayList<>();
-        arrayResults.addAll(Arrays.asList(getResources().getStringArray(R.array.results)));
-
-        adapter = new ArrayAdapter<String>(
-                MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                arrayResults
-        );
-
-        search_results.setAdapter(adapter);
     } // end function
 } // end MainActivity
