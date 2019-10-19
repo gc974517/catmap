a145251831ad99a54a9a3ac7fa176bce28f4cf77
Admin <gc974517@ohio.edu>
2019-10-19 16:43:42 -0400

    Intermediate push. Small UI changes in main project folder.

--- catmap/app/src/main/java/com/example/catmap/MainActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.catmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView search_results;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        search_results = (ListView) findViewById(R.id.search_results);
+
+        ArrayList<String> arrayResults = new ArrayList<>();
+        arrayResults.addAll(Arrays.asList(getResources().getStringArray(R.array.results)));
+
+        adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayResults
+        );
+
+        search_results.setAdapter(adapter);
+    } // end function
+} // end MainActivity
