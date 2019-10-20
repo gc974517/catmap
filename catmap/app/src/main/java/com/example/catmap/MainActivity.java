77fbbc8bfbebcb7c5e02f9c6bcdd02374a714180
Admin <gc974517@ohio.edu>
2019-10-19 20:22:33 -0400

    Added test variable in MainActivity to check SDK importing.

--- catmap/app/src/main/java/com/example/catmap/MainActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MainActivity.java
@@ -10,15 +10,24 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.SearchView;
 
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private final int CODE_PERMISSIONS = 1;
+    private IALocationManager mIALocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIALocationManager = IALocationManager.create(this);
 
         String[] neededPermissions = {
                 Manifest.permission.CHANGE_WIFI_STATE,
