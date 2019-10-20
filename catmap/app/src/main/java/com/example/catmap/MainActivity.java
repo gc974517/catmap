44a213670082782a00ddfe664e95258483941a18
Admin <gc974517@ohio.edu>
2019-10-19 20:15:56 -0400

    Changed status bar color to match theme. Added constraints to elements for device compatability. IndoorAtlas SDK integrated & compiles.

--- catmap/app/src/main/java/com/example/catmap/MainActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.catmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,9 +13,23 @@ import android.widget.SearchView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private final int CODE_PERMISSIONS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] neededPermissions = {
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        ActivityCompat.requestPermissions(this, neededPermissions, CODE_PERMISSIONS);
+    } // end function
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     } // end function
 } // end MainActivity
