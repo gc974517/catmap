09d8a888845ce692b745f107f18558166b98b3c9
mm587517 <marcelomoralesuni@gmail.com>
2019-10-20 22:23:35 -0400

    implementing floor detection

--- catmap/app/src/main/java/com/example/catmap/MapsActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MapsActivity.java
@@ -2,15 +2,21 @@ package com.example.catmap;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -21,11 +27,28 @@ import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
 import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+import com.squareup.picasso.Target;
 
 public class MapsActivity extends FragmentActivity implements IALocationListener, OnMapReadyCallback {
     private GoogleMap mMap;
     private IALocationManager mIALocationManager;
     private Marker mMarker;
+    private final static float DEFAULT_ZOOM=17.0f;
+    private static final int MAX_DIMENSION = 2048;
+    private final static String TAG="CATMAP";
+    private GroundOverlay mGroundOverlay = null;
+
+    IARegion mCurrentVenue = null;
+    IARegion mCurrentFloorPlan = null;
+    Integer mCurrentFloorLevel = null;
+    Float mCurrentCertainty = null;
+    private Target mLoadTarget;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +74,7 @@ public class MapsActivity extends FragmentActivity implements IALocationListener
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         if (mMarker == null)
             if (mMap != null)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
         else
             mMarker.setPosition(latLng);
     }
@@ -82,4 +105,92 @@ public class MapsActivity extends FragmentActivity implements IALocationListener
         mIALocationManager.destroy();
         super.onDestroy();
     }
+
+
+
+    public void onEnterRegion(IARegion region) {
+        if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+            // triggered when entering the mapped area of the given floor plan
+            Log.d(TAG, "Entered " + region.getName());
+            Log.d(TAG, "floor plan ID: " + region.getId());
+            mCurrentFloorPlan = region;
+        }
+        else if (region.getType() == IARegion.TYPE_VENUE) {
+            // triggered when near a new location
+            Log.d(TAG, "Location changed to " + region.getId());
+            mCurrentVenue=region;
+        }
+    }
+
+
+    public void onExitRegion(IARegion region) {
+        // leaving a previously entered region
+        if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+            mCurrentFloorPlan = null;
+            // notice that a change of floor plan (e.g., floor change)
+            // is signaled by an exit-enter pair so ending up here
+            // does not yet mean that the device is outside any mapped area
+        }
+    }
+
+    private void handleFloorPlanChange(final IAFloorPlan newFloorPlan) {
+
+        final String url = newFloorPlan.getUrl();
+        mLoadTarget = new Target() {
+
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Log.d(TAG, "onBitmap loaded with dimensions: " + bitmap.getWidth() + "x"
+                        + bitmap.getHeight());
+                if (mCurrentFloorPlan != null && newFloorPlan.getId().equals(mCurrentFloorPlan.getId())) {
+                    setupGroundOverlay(newFloorPlan, bitmap);
+                }
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                // N/A
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable placeHolderDrawable) {
+                mCurrentFloorPlan = null;
+            }
+        };
+
+        RequestCreator request = Picasso.with(this).load(url);
+
+        final int bitmapWidth = newFloorPlan.getBitmapWidth();
+        final int bitmapHeight = newFloorPlan.getBitmapHeight();
+
+        if (bitmapHeight > MAX_DIMENSION) {
+            request.resize(0, MAX_DIMENSION);
+        } else if (bitmapWidth > MAX_DIMENSION) {
+            request.resize(MAX_DIMENSION, 0);
+        }
+
+        request.into(mLoadTarget);
+    }
+
+
+    private void setupGroundOverlay(IAFloorPlan floorPlan, Bitmap bitmap) {
+
+        if (mGroundOverlay != null) {
+            mGroundOverlay.remove();
+        }
+
+        if (mMap != null) {
+            BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+            IALatLng iaLatLng = floorPlan.getCenter();
+            LatLng center = new LatLng(iaLatLng.latitude, iaLatLng.longitude);
+            GroundOverlayOptions fpOverlay = new GroundOverlayOptions()
+                    .image(bitmapDescriptor)
+                    .zIndex(0.0f)
+                    .position(center, floorPlan.getWidthMeters(), floorPlan.getHeightMeters())
+                    .bearing(floorPlan.getBearing());
+
+            mGroundOverlay = mMap.addGroundOverlay(fpOverlay);
+        }
+    }
+
 }
