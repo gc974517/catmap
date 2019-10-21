237af00367ec2f296b27c304fd96f1b85b78ea1f
Admin <gc974517@ohio.edu>
2019-10-21 17:41:56 -0400

    Added naive indoor wayfinding and manual destination marking.

--- catmap/app/src/main/java/com/example/catmap/MapsActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MapsActivity.java
@@ -26,12 +26,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
 import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.IARoute;
+import com.indooratlas.android.sdk.IAWayfindingListener;
+import com.indooratlas.android.sdk.IAWayfindingRequest;
 import com.indooratlas.android.sdk.resources.IAFloorPlan;
 import com.indooratlas.android.sdk.resources.IALatLng;
 import com.indooratlas.android.sdk.resources.IALocationListenerSupport;
@@ -40,28 +45,40 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
 import com.squareup.picasso.Target;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements LocationListener, OnMapReadyCallback {
     private static final int MAX_DIMENSION = 2048;
 
     private GoogleMap mMap;
-    private Marker mMarker;
+    private List<Polyline> mPolylines = new ArrayList<>();
+    private Marker mDestinationMarker;
 
     private Target mTarget;
 
     private boolean mCameraUpdate = true;
-    private boolean mShowIndoor = false;
+    private boolean mIndoorLock = false;
 
     private IALocationManager mIALocationManager;
     private IARegion mVenue = null;
     private IARegion mFloorPlan = null;
-    private Integer mFloorLevel = null;
+    private int mFloorLevel;
     private GroundOverlay mGroundOverlay = null;
+    private IARoute mRoute;
+
     private IALocationListener mListener = new IALocationListenerSupport() {
         @Override
         public void onLocationChanged(IALocation location) {
             if (mMap == null) return;
 
-            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            final int newFloorLevel = location.getFloorLevel();
+            if (mFloorLevel != newFloorLevel)
+                updateRoute();
+            mFloorLevel = newFloorLevel;
+
             if (mCameraUpdate) {
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.5f));
                 mCameraUpdate = false;
@@ -72,9 +89,12 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     private IARegion.Listener mRegionListener = new IARegion.Listener() {
         @Override
         public void onEnterRegion(IARegion region) {
-            if (region.getType() == IARegion.TYPE_VENUE)
+            if (region.getType() == IARegion.TYPE_VENUE) {
                 mVenue = region;
-            else if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+
+                mIALocationManager.lockIndoors(false);
+                mIndoorLock = false;
+            } else if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
                 if (mGroundOverlay == null || !region.equals(mFloorPlan)) {
                     mCameraUpdate = true;
 
@@ -130,20 +150,53 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
                     mGroundOverlay.setTransparency(0.0f);
                 }
 
-                mShowIndoor = true;
+                mIALocationManager.lockIndoors(true);
+                mIndoorLock = true;
             }
         }
 
         @Override
         public void onExitRegion(IARegion region) {
-            if (region.getType() == IARegion.TYPE_VENUE)
+            if (region.getType() == IARegion.TYPE_VENUE) {
                 mVenue = region;
-            else if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+
+                mIALocationManager.lockIndoors(false);
+                mIndoorLock = false;
+            } else if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
                 if (mGroundOverlay != null)
                     mGroundOverlay.setTransparency(0.5f);
 
-                mShowIndoor = false;
+                mIALocationManager.lockIndoors(true);
+                mIndoorLock = true;
             }
+        }
+    };
+
+    private IAWayfindingRequest mWayfindingDestination;
+    private IAWayfindingListener mWayfindingListener = new IAWayfindingListener() {
+        @Override
+        public void onWayfindingUpdate(IARoute route) {
+            mRoute = route;
+
+            boolean hasArrived;
+            if (route.getLegs().size() == 0) {
+                hasArrived = false;
+            } else {
+                final double FINISH_THRESHOLD_METERS = 8.0;
+                double routeLength = 0;
+                for (IARoute.Leg leg : route.getLegs())
+                    routeLength += leg.getLength();
+
+                hasArrived = routeLength < FINISH_THRESHOLD_METERS;
+            }
+
+            if (hasArrived) {
+                mRoute = null;
+                mWayfindingDestination = null;
+                mIALocationManager.removeWayfindingUpdates();
+            }
+
+            updateRoute();
         }
     };
 
@@ -175,9 +228,22 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             public void onMapClick(LatLng pos) {
-                MarkerOptions markerOptions = new MarkerOptions().position(pos);
-                mMap.clear();
-                mMap.addMarker(markerOptions);
+                if (mMap != null) {
+                    mWayfindingDestination = new IAWayfindingRequest.Builder()
+                        .withFloor(mFloorLevel)
+                        .withLatitude(pos.latitude)
+                        .withLongitude(pos.longitude)
+                        .build();
+
+                    mIALocationManager.requestWayfindingUpdates(mWayfindingDestination, mWayfindingListener);
+
+                    if (mDestinationMarker == null)
+                        mDestinationMarker = mMap.addMarker(new MarkerOptions()
+                            .position(pos)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                    else
+                        mDestinationMarker.setPosition(pos);
+                }
             }
         });
     }
@@ -204,16 +270,24 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     @Override
     protected void onResume() {
         super.onResume();
+
         mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mListener);
         mIALocationManager.registerRegionListener(mRegionListener);
+
+        if (mWayfindingDestination != null)
+            mIALocationManager.requestWayfindingUpdates(mWayfindingDestination, mWayfindingListener);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
         if (mIALocationManager != null) {
             mIALocationManager.removeLocationUpdates(mListener);
             mIALocationManager.registerRegionListener(mRegionListener);
+
+            if (mWayfindingDestination != null)
+                mIALocationManager.removeWayfindingUpdates();
         }
     }
 
@@ -226,5 +300,30 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
             this.getFragmentManager().beginTransaction().remove(mapFragment).commit();
 
         mIALocationManager.destroy();
+    }
+
+    private void updateRoute() {
+        for (Polyline pl : mPolylines)
+            pl.remove();
+        mPolylines.clear();
+
+        if (mRoute == null)
+            return;
+
+        for (IARoute.Leg leg : mRoute.getLegs()) {
+            if (leg.getEdgeIndex() == null)
+                continue;
+
+            PolylineOptions polylineOptions = new PolylineOptions()
+                    .add(new LatLng(leg.getBegin().getLatitude(), leg.getBegin().getLongitude()))
+                    .add(new LatLng(leg.getEnd().getLatitude(), leg.getEnd().getLongitude()));
+
+            if (leg.getBegin().getFloor() == mFloorLevel && leg.getEnd().getFloor() == mFloorLevel)
+                polylineOptions.color(0xFF0000FF);
+            else
+                polylineOptions.color(0x300000FF);
+
+            mPolylines.add(mMap.addPolyline(polylineOptions));
+        }
     }
 }
