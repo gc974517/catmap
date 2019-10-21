30387b6467b6f1e4e7442c711e075c8522a70fff
Admin <gc974517@ohio.edu>
2019-10-20 21:22:58 -0400

    Refactor of current Google Maps implementation.

--- catmap/app/src/main/java/com/example/catmap/MapsActivity.java
+++ catmap/app/src/main/java/com/example/catmap/MapsActivity.java
@@ -20,62 +20,53 @@ import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
 
 public class MapsActivity extends FragmentActivity implements IALocationListener, OnMapReadyCallback {
-    private IALocationManager mIALocationManager;
     private GoogleMap mMap;
+    private IALocationManager mIALocationManager;
     private Marker mMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mIALocationManager = IALocationManager.create(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        mIALocationManager = IALocationManager.create(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
     }
 
-    // Declare IA location listener, define update operations.
+    @Override
     public void onLocationChanged(IALocation location) {
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-
-        if (mMarker == null) {
-            if (mMap != null) {
-                mMarker = mMap.addMarker(new MarkerOptions().position(latLng)
-                        .icon(BitmapDescriptorFactory.defaultMarker(200.0f)));
+        if (mMarker == null)
+            if (mMap != null)
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
-            }
-        } else {
+        else
             mMarker.setPosition(latLng);
-        }
     }
 
-    // On resuming the app, begin requesting location updates.
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // Empty.
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         mIALocationManager.requestLocationUpdates(IALocationRequest.create(), this);
     }
 
-    // Stop location requests on app pause.
     @Override
     protected void onPause() {
         super.onPause();
@@ -84,17 +75,11 @@ public class MapsActivity extends FragmentActivity implements IALocationListener
     }
 
     public void onDestoy() {
-        mIALocationManager.destroy();
-
         MapFragment mapFragment = (MapFragment) this.getFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null)
             this.getFragmentManager().beginTransaction().remove(mapFragment).commit();
 
+        mIALocationManager.destroy();
         super.onDestroy();
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // N/A
     }
 }
